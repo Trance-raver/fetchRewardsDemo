88ffd53b0e107cd7198176221e205fd93eba43d9
Akshay <guptaakshay908@gmail.com>
2021-05-21 11:22:48 -0500

    Done

---
+++ src/main/java/com/route/fetchRewards/dto/SpendResponse.java
@@ -0,0 +1,22 @@
+package com.route.fetchRewards.dto;
+
+public class SpendResponse {
+    String payer;
+    int points;
+
+    public String getPayer() {
+        return payer;
+    }
+
+    public void setPayer(String payer) {
+        this.payer = payer;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
